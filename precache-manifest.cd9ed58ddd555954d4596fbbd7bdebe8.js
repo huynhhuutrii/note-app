6d2e50ff5064c062e1f9910f5c6bd7e87eba010a
Huỳnh Trí <huynhtri2k11@gmail.com>
2020-05-01 23:17:52 +0700

    Updates

---
+++ precache-manifest.cd9ed58ddd555954d4596fbbd7bdebe8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faac3a21eb25fd11926661915bf334da",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "9ab68b053d7c767d83a9",
+    "url": "/note-app/static/css/main.2e5758f3.chunk.css"
+  },
+  {
+    "revision": "57fcb9781572ca654760",
+    "url": "/note-app/static/js/2.e1df951f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/note-app/static/js/2.e1df951f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ab68b053d7c767d83a9",
+    "url": "/note-app/static/js/main.083e1849.chunk.js"
+  },
+  {
+    "revision": "382640c346095741cb82",
+    "url": "/note-app/static/js/runtime-main.8d446e83.js"
+  },
+  {
+    "revision": "d0f5911d9a71bc7ec61eff8a4f25ea4e",
+    "url": "/note-app/static/media/add.d0f5911d.svg"
+  },
+  {
+    "revision": "650cdc57dc0be3a71cc230a760215df5",
+    "url": "/note-app/static/media/delete.650cdc57.svg"
+  },
+  {
+    "revision": "cbe4df50863e83bdbc77300af09479cc",
+    "url": "/note-app/static/media/noteIcon.cbe4df50.svg"
+  },
+  {
+    "revision": "27b127d30bccd3bb2efd092d4309e9f6",
+    "url": "/note-app/static/media/pen.27b127d3.svg"
+  },
+  {
+    "revision": "89a8ab4460c75a7ebd2d4a766efc287c",
+    "url": "/note-app/static/media/signing.89a8ab44.svg"
+  },
+  {
+    "revision": "c412776699c042548bfc30e7b2fa85ca",
+    "url": "/note-app/static/media/tools-and-utensils.c4127766.svg"
+  }
+]);
